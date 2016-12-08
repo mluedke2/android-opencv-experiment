47eee658d5e2228481bb3fb45691e6a54d5be6ea
Matt Luedke <mluedke2@gmail.com>
2016-12-08 15:18:37 -0800

    process image w blur

--- openCVSamplecameracalibration/src/main/java/org/opencv/samples/cameracalibration/SomeOtherActivity.java
+++ openCVSamplecameracalibration/src/main/java/org/opencv/samples/cameracalibration/SomeOtherActivity.java
@@ -5,10 +5,19 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.ImageView;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -29,6 +38,24 @@ public class SomeOtherActivity extends Activity {
   @BindView(R.id.img_edited)
   ImageView editedImageView;
 
+
+  private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+    @Override
+    public void onManagerConnected(int status) {
+      switch (status) {
+        case LoaderCallbackInterface.SUCCESS: {
+          Log.e("MYTEST", "OpenCV loaded successfully");
+          processAndLoadIfNeeded();
+        }
+        break;
+        default: {
+          super.onManagerConnected(status);
+        }
+        break;
+      }
+    }
+  };
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -36,6 +63,18 @@ public class SomeOtherActivity extends Activity {
     setContentView(R.layout.activity_some_other);
 
     ButterKnife.bind(this);
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    if (!OpenCVLoader.initDebug()) {
+      Log.e("MYTEST", "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+      OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+    } else {
+      Log.e("MYTEST", "OpenCV library found inside package. Using it!");
+      mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+    }
   }
 
   @OnClick(R.id.btn_camera)
@@ -48,6 +87,7 @@ public class SomeOtherActivity extends Activity {
     if (requestCode == CAMERA_PIC_REQUEST) {
       Bitmap image = (Bitmap) data.getExtras().get("data");
       originalImageView.setImageBitmap(image);
+
     } else if (requestCode == PICK_IMAGE) {
       Uri selectedImage = data.getData();
       String[] filePathColumn = { MediaStore.Images.Media.DATA };
@@ -72,5 +112,38 @@ public class SomeOtherActivity extends Activity {
     intent.setType("image/*");
     intent.setAction(Intent.ACTION_GET_CONTENT);
     startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+  }
+
+  @OnClick(R.id.btn_process)
+  public void process() {
+    processAndLoadIfNeeded();
+  }
+
+  private void processAndLoadIfNeeded() {
+
+    BitmapDrawable drawable = (BitmapDrawable) originalImageView.getDrawable();
+
+    if (drawable != null) {
+      Bitmap bitmap = drawable.getBitmap();
+
+      if (bitmap != null) {
+
+        Mat mat = new Mat();
+
+        Utils.bitmapToMat(bitmap, mat);
+
+        Mat outmat = new Mat();
+
+        Imgproc.blur(mat, outmat, mat.size());
+
+        Bitmap newImage = Bitmap.createBitmap(
+          (int)mat.size().width,
+          (int)mat.size().height, Bitmap.Config.ARGB_8888);
+
+        Utils.matToBitmap(outmat, newImage);
+
+        editedImageView.setImageBitmap(newImage);
+      }
+    }
   }
 }
